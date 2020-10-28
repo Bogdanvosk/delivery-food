ae72ce1049a13e2b9aac9de2e200d87f683e532c
unknown <bogdanvoskanov@gmail.com>
2020-10-28 18:42:23 +0300

    Global changes in styles

--- js/main.js
+++ js/main.js
@@ -1,15 +1,47 @@
-'use strict'
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
 
-const modal = document.querySelector('.modal')
-const closeButton = document.querySelector('.modal-close')
-const cartButton = document.querySelector('#cart-button')
-const cancelButton = document.querySelector('#modal-cancel')
 
-const toggleModal = () => {
-    modal.classList.toggle('active')
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+   modal.classList.toggle("is-open");
 }
 
-cartButton.addEventListener('click', toggleModal)
-closeButton.addEventListener('click', toggleModal)
-cancelButton.addEventListener('click', toggleModal)
+// * day 1
 
+const buttonAuth = document.querySelector(".button-auth");
+const modalAuth = document.querySelector(".modal-auth");
+const closeAuth = document.querySelector(".close-auth");
+const logInForm = document.querySelector("#logInForm");
+
+let login = '';
+
+function toggleModalAuth() {
+   modalAuth.classList.toggle("is-open");
+}
+
+function authorized() {
+   console.log("Авторизован");
+}
+
+function notAuthorized() {
+   console.log("Не авторизован");
+
+   function logIn(e) {
+      e.preventDefault();
+      console.log('Логин');
+   }
+
+   buttonAuth.addEventListener("click", toggleModalAuth);
+   closeAuth.addEventListener("click", toggleModalAuth);
+   logInForm.addEventListener("submit", logIn)
+}
+
+if (login) {
+   authorized();
+} else {
+   notAuthorized();
+}
